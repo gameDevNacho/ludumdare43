d35c66e11b56fda58f701182283993b3042799f9
Diegolo <dmanzano101@gmail.com>
2018-12-01 17:40:37 +0100

    Crate / Products
    Scriptable OBJ

---
+++ LudumDare43/Assets/Scripts/Crate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crate : MonoBehaviour {
+
+    public enum Type { Small = 5, Medium = 10, Large = 20 };
+
+    private int weight;
+
+    public Type myType;
+
+    public Product myProduct;
+
+    private AudioSource myAudioSource;
+
+    void Start ()
+    {
+        weight = (int)myType;
+
+        Debug.Log(weight);
+        Debug.Log(myProduct.nameText);
+
+        myAudioSource = GetComponent<AudioSource>();
+        myAudioSource.clip = myProduct.shakeSound;
+
+        myAudioSource.Play();
+    }
+
+
+    void Update ()
+    {
+		
+	}
+}
---
+++ LudumDare43/Assets/Scripts/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "SO_Products")]
+public class Product : ScriptableObject
+{
+
+    public string nameText;
+    public int price;
+    public AudioClip shakeSound;
+    
+
+}
