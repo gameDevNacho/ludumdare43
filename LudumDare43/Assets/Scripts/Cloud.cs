c0fddcbc36608c14b1b292c6b72222d12d79d459
Nacho <ignacio.gamedeveloper@gmail.com>
2018-12-02 23:32:06 +0100

    Clouds

---
+++ LudumDare43/Assets/Scripts/Cloud.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    [SerializeField]
+    private float max;
+    [SerializeField]
+    private float min;
+
+    private float speed;
+
+	void Start ()
+    {
+        speed = Random.Range(min, max);
+	}
+	
+	void Update ()
+    {
+        transform.Translate(-transform.forward * speed * Time.deltaTime);	
+	}
+}
---
+++ LudumDare43/Assets/Scripts/CloudManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform point;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<Cloud>())
+        {
+            other.transform.position = new Vector3(other.transform.position.x, other.transform.position.y, point.position.z);
+        }
+    }
+
+
+}
