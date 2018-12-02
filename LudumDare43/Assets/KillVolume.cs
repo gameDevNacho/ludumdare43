3873fec14e0796c46e9752ce103c4fcdb53cae80
PartnerBros <38180754+PartnerBros@users.noreply.github.com>
2018-12-02 20:09:13 +0100

    commit

---
+++ LudumDare43/Assets/KillVolume.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using
+
+[RequireComponent(typeof(BoxCollider))]
+public class KillVolume : MonoBehaviour {
+
+    BoxCollider collider;
+
+    private void Awake()
+    {
+        collider = this.GetComponent<BoxCollider>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
---
+++ LudumDare43/Assets/Scripts/IKillerVolume.cs
@@ -0,0 +1,6 @@
+﻿
+
+public interface IKvolume
+{
+     void OnKillVolumeEnter();
+}
