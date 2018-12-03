f333e2b52461ec8b03d3fd803c80be2cbaf276b9
PartnerBros <38180754+PartnerBros@users.noreply.github.com>
2018-12-03 14:06:34 +0100

    menuPrincipal

--- LudumDare43/Assets/CacaThrower.cs
+++ LudumDare43/Assets/CacaThrower.cs
@@ -8,20 +8,36 @@ public class CacaThrower : MonoBehaviour {
     int index;
     GameObject current;
 
+    [SerializeField]
+    private float minTime = 2.00f;
+    [SerializeField]
+    private float maxTime = 4.00f;
+
+    private float currentTime = 0.00f;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        if(currentTime <= 0.00f)
+        {
+            LaunchObject();
+            currentTime = Random.Range(minTime, maxTime);
+        }
+        else
+        {
+            currentTime -= Time.deltaTime;
+        }
+    }
 
     private void LaunchObject()
     {
         index = Random.Range(0, cacas.Length);
-        current = Instantiate<GameObject>(cacas[index]);
+        current = Instantiate(cacas[index]);
         current.GetComponent<BoxComponent>().AddSuccionAceleration();
     }
 }
